1a2be7b85b5bfbba70bf36c252a3f9f487303163
nosyeih <hieysonshc@gmail.com>
2026-05-06 17:11:16 -0500

    Improve monitor websockets for consult orders

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,8 @@ ASGI_APPLICATION = 'config.asgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=60,
+        conn_max_age=30,
+        conn_health_checks=True,
     )
 }
 
